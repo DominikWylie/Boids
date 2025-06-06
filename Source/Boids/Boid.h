258d569b32ebf0808227441fff87adcf8a984f7c
DOMINIK WYLIE <1703468@uad.ac.uk>
2025-06-06 12:21:07 +0100

    plugin headers sucessfully added to project code files through build cs

--- Source/Boids/Boid.h
+++ Source/Boids/Boid.h
@@ -4,7 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-//#include "Octree"
+#include "OctreeInterface.h"
 
 
 #include "Boid.generated.h"
--- Source/Boids/Boids.Build.cs
+++ Source/Boids/Boids.Build.cs
@@ -8,9 +8,11 @@ public class Boids : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Octree"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		PublicIncludePaths.AddRange(new string[] { "Octree/Public", "Octree/Private" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
