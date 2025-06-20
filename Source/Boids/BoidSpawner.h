3f7ff0f82fac2d64bac572b8d1276187d0350f13
DOMINIK WYLIE <1703468@uad.ac.uk>
2025-06-20 14:38:31 +0100

    finished vector math for octreemain

--- Source/Boids/BoidSpawner.h
+++ Source/Boids/BoidSpawner.h
@@ -24,7 +24,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	void SpawnTimer();
+	void Spawn();
 
 	FTimerHandle MyTimerHandle;
 	UWorld* World;
